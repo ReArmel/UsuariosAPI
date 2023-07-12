03a8ff1046b914fbcc8a56d71a0e33738967b594
ReArmel <rearmel@icloud.com>
2023-07-12 10:47:08 -0400

    Iniciando o projeto criando o controller, model e profile

---
+++ UsuariosAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using UsuariosAPI.Database.Dtos;
+
+namespace UsuariosAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastraUsuario(CreateUsuarioDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
