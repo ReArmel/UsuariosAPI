aec00b89fc00db36ea4bef3dbccb063b04d448c2
ReArmel <rearmel@icloud.com>
2023-07-12 11:57:31 -0400

    Configurando o db e o identity

--- UsuariosAPI/Models/Usuario.cs
+++ UsuariosAPI/Models/Usuario.cs
@@ -1,6 +1,11 @@
-﻿namespace UsuariosAPI.Models
+﻿using Microsoft.AspNetCore.Identity;
+using UsuariosAPI.Database;
+
+namespace UsuariosAPI.Models
 {
-    public class Usuario
+    public class Usuario : IdentityUser
     {
+        public DateTime DataNascimento { get; set; }
+        public Usuario() : base() { }
     }
 }
--- UsuariosAPI/Program.cs
+++ UsuariosAPI/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UsuariosAPI.Database;
+using UsuariosAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectString = builder.Configuration.GetConnectionString("UsuarioConnection");
+
+builder.Services.AddDbContext<UsuarioDbContext>
+    (opts =>
+    {
+        opts.UseMySql(connectString, ServerVersion.AutoDetect(connectString));
+    });
+
+builder.Services
+    .AddIdentity<Usuario, IdentityRole>()
+    .AddEntityFrameworkStores<UsuarioDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
