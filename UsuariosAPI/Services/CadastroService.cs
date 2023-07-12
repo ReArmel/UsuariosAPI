f04a56a24983dafe98ddbe87ef40b89fc2a9ca9c
ReArmel <rearmel@icloud.com>
2023-07-12 17:26:54 -0400

    Iniciando a parte de login

--- UsuariosAPI/Services/CadastroService.cs
+++ UsuariosAPI/Services/CadastroService.cs
@@ -5,15 +5,17 @@ using UsuariosAPI.Models;
 
 namespace UsuariosAPI.Services
 {
-    public class CadastroService
+    public class UsuarioService
     {
         private IMapper _mapper;
         private UserManager<Usuario> _userManager;
+        private SignInManager<Usuario> _signinManager;
 
-        public CadastroService(UserManager<Usuario> userManager, IMapper mapper)
+        public UsuarioService(UserManager<Usuario> userManager, IMapper mapper, SignInManager<Usuario> signinManager)
         {
             _userManager = userManager;
             _mapper = mapper;
+            _signinManager = signinManager;
         }
 
         public async Task Cadastra(CreateUsuarioDto dto)
@@ -23,8 +25,19 @@ namespace UsuariosAPI.Services
             IdentityResult resultado = await _userManager.CreateAsync(usuario, dto.Password);
 
             if (!resultado.Succeeded)
+            {
+                throw new ApplicationException("Falha ao cadastrar usuário");
+            }
+        }
 
-            throw new ApplicationException("Falha ao cadastrar usuário");
+        public async Task Login(LoginUsuarioDto dto)
+        {
+            var resultado = await _signinManager.PasswordSignInAsync(dto.Username, dto.Password, false, false);
+            
+            if (!resultado.Succeeded)
+            {
+                throw new ApplicationException("Usuário não autenticado!");
+            }
         }
     }
 }
